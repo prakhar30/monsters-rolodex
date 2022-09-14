72da350dd5b15e82f3bb7b2c346c233b1f91ad81
Prakhar Tripathi <prakhar.tripathi@cred.club>
2022-09-14 18:22:26 +0530

    converted app to a class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Prakhar from state",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi {this.state.name}</p>
+          <button
+            onClick={() => {
+              // this.setState can also be called as this.setState((state, props) => {}, () => {} ); 
+              // where the second argument is the completion callback
+              this.setState({ name: "Prakhar from JS" }); // this is async for react
+            }}
+          >
+            Change name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
