d9ca21c3a2db1dfa82bf0f12ad0b47c86209ff5b
Prakhar Tripathi <prakhar.tripathi@cred.club>
2022-09-18 18:31:52 +0530

    moved card to its own component

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ class App extends Component {
     return (
       <div className="App">
 
+        <h1 className="app-title"> Monsters </h1>
         <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters' className='monsters-search-box'/>
         <CardList monsters={fileteredMonsters}/>
 
