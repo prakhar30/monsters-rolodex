c0220492c90a561948f7f07d55bf9024cc111579
Prakhar Tripathi <prakhar.tripathi@cred.club>
2022-09-18 18:14:21 +0530

    styled the app

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
     return (
       <div className="App">
 
-        <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters' className='search-box'/>
+        <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters' className='monsters-search-box'/>
         <CardList monsters={fileteredMonsters}/>
 
       </div>
